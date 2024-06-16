f77d7243d3f648fb5a75b986499caf63d7fdf792
Travis Deshotels <travis.deshotels@gmail.com>
2024-06-16 10:21:31 -0500

    Add status codes

--- api/src/main.py
+++ api/src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, Response, status
 from pydantic import BaseModel
 from typing import Annotated
 from exception.invalidguessexception import InvalidGuessException
@@ -14,22 +14,30 @@ class GameUser(BaseModel):
     email: str
 
 
-@app.post("/register")
-def register(game_user: GameUser):
+@app.post("/register", status_code=201)
+def register(game_user: GameUser, response: Response):
     secret_id = service.register_new_user(game_user.userName, game_user.email)
-    return {
-        "message": "Registration complete. Use secret_id to play.",
-        "secretId": secret_id
-    }
+    if secret_id:
+        return {
+            "message": "Registration complete. Use secret_id to play.",
+            "secretId": secret_id
+        }
+    else:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            "message": "Bad request."
+        }
 
 
-@app.get("/")
-def play(secret: Annotated[str | None, Header()] = None, guess: int = 11):
+@app.get("/", status_code=200)
+def play(response: Response, secret: Annotated[str | None, Header()] = None, guess: int = 11):
     try:
         if secret is None:
+            response.status_code = status.HTTP_400_BAD_REQUEST
             return {"message": "Missing secret id."}
         return service.play(secret, guess)
     except BadDataException:
+        response.status_code = status.HTTP_400_BAD_REQUEST
         return {
             "message": "Bad request."
         }
@@ -39,6 +47,6 @@ def play(secret: Annotated[str | None, Header()] = None, guess: int = 11):
         }
 
 
-@app.get("/scores")
+@app.get("/scores", status_code=200)
 def get_scores():
     return service.scoreboard()

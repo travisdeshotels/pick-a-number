b987d4f6273b96abaa2ea386178c63e458e21632
Travis Deshotels <travis.deshotels@gmail.com>
2024-06-15 21:35:07 -0500

    Register via terminal

--- terminal/src/main.py
+++ terminal/src/main.py
@@ -1,10 +1,20 @@
 import os
+import json
 import requests
 from lib.colors import blue, cyan, green, yellow, underline, bold_blue, bold_yellow, fail
+from exception.badinputexception import BadInputException
 
 
 class PlayerConfig:
     filename = ".player_config"
+    url = ''
+
+    def register(self):
+        user_name = input("Enter user name: ")
+        email = input("Enter email address: ")
+        response = requests.post(url=f'{self.url}/register',
+                                 data=json.dumps({'userName': user_name, 'email': email}))
+        return response.json()['secretId']
 
     def is_player_configured(self):
         return os.path.isfile(self.filename)
@@ -13,12 +23,22 @@ class PlayerConfig:
         with open(self.filename, "r") as config:
             return config.read().split(",")
 
-    def create_config_file(self):
-        with open(self.filename, "w") as config:
-            url = input("Enter host url: ")
+    def get_secret_id(self):
+        register_choice = input("(R)egister\n(E)nter secret id: ")
+        if register_choice == 'R':
+            secret_id = self.register()
+        elif register_choice == 'E':
             secret_id = input("Enter your secret_id: ")
-            config.write(f"{url},{secret_id}")
-            return url, secret_id
+        else:
+            raise BadInputException("Invalid choice was entered!")
+        return secret_id
+
+    def create_config_file(self):
+        self.url = input("Enter host url: ")
+        secret_id = self.get_secret_id()
+        with open(self.filename, "w") as config:
+            config.write(f"{self.url},{secret_id}")
+            return self.url, secret_id
 
     def get_config_values(self):
         if self.is_player_configured():
@@ -34,8 +54,8 @@ def print_result(result):
     print()
 
 
-def print_stats(stats):
-    print(underline('Your stats'))
+def print_stats(stats, username):
+    print(underline(f'{username} stats'))
     print(f"wins: {green(str(stats['wins']))}")
     print(f"most guessed: {yellow(str(stats['mostGuessed']))}")
     print(f"total guesses: {bold_blue(str(stats['total']))}")
@@ -57,7 +77,7 @@ class PlayHelper:
             guess = int(input('Enter a number between 1 and 10: '))
         data = requests.get(headers={'Secret': self.secret_id}, url=f'{self.game_url}/?guess={guess}').json()
         print_result(data['result'])
-        print_stats(data['stats'])
+        print_stats(data['stats'], data['username'])
 
     def scoreboard(self):
         scores = requests.get(f'{self.game_url}/scores').json()
@@ -68,7 +88,7 @@ class PlayHelper:
 
 
 play_helper = PlayHelper()
-prompt = f'({bold_yellow("p")})lay\n({bold_yellow("s")})coreboard\n({bold_yellow("q")})uit\n'
+prompt = f'({bold_yellow("p")})lay\n({bold_yellow("s")})coreboard\n({bold_yellow("q")})uit\n% '
 choice = input(prompt)
 while choice != 'q':
     if choice == 's':
